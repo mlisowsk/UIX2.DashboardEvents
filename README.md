33afeb5f2cc5ecd18bab33649e6dfced6c108aa5
Martin Lisowski <martin.j.lisowski@gmail.com>
2024-11-14 10:24:08 +0100

    v0.3: Added ShellListing events.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UIX2.DashboardEvents
 UIX v2 sample application, ShellFrame & Listing Dashboard
-Logging calls to Dashboard event handlers to the browser console.
+Logging calls to Dashboard and Listing event handlers to the browser console.
 
 ## Build
 
@@ -12,6 +12,14 @@ To automatically deploy to an M-Files vault, you need to either
 2. edit the `install-application.ps1` located there, adding vault and admin credentials.
 
 In case 1, the deployment group can be set in the VS project file, for example `<MFilesVaultGroup>test</MFilesVaultGroup>` will deploy to vaults in the group `test` after building.
+
+## Use
+
+1. Open the browser devtools by pressing F12.
+2. Navigate to a view.
+3. Select "Show DashboardEvents dashboard" from the top menu to display the dashboard.
+
+Optionally, select an object or view before opening the dashboard, then edit that object in the metadata card and observe events.
 
 ### Source
 Forked from source: https://developer.m-files.com/Frameworks/User-Interface-Extensibility-Framework/Reference/Version2/Samples/ShellFrameAndDashboard/
--- UIX2.Dashboard/app/dashboard.js
+++ UIX2.Dashboard/app/dashboard.js
@@ -15,8 +15,8 @@ window.OnNewDashboard = (dashboard) => {
 		console.log("Dashboard.Stop");
 	}
 
-	function handleCustomDataChanged() {
-		console.log("Dashboard.CustomDataChanged");
+	function handleCustomDataChanged(data) {
+		console.log("Dashboard.CustomDataChanged, data=", data);
 	}
 
 	function handleDashboardRefresh() {
@@ -40,4 +40,126 @@ window.OnNewDashboard = (dashboard) => {
 		hdlDBRefresh = handle;
 		console.log("registered hdlDBRefresh = " + handle);
 	});
+
+	// ShellListing events:
+
+	function handleListingContentChanged(items) {
+		console.log("Listing.ContentChanged. Items", items);
+		document.getElementById("curTime").innerHTML = new Date().toISOString();
+	}
+	function handleListingListingActivated() {
+		console.log("Listing.ListingActivated");
+	}
+	function handleListingListingDeactivated() {
+		console.log("Listing.ListingDeactivated");
+	}
+
+	function handleListingListItemAdded(objectVersion) {
+		console.log("Listing.ListItemAdded. objectVersion", objectVersion);
+	}
+	function handleListingListItemModified(oldServerObjVer, newObjVer) {
+		console.log("Listing.ListItemModified. oldServerObjVer", oldServerObjVer, "newObjVer", newObjVer);
+	}
+	function handleListingListItemRemoved(listItem) {
+		console.log("Listing.ListItemRemoved. listItem",listItem);
+	}
+
+	function handleListingSelectedItemsChanged(items) {
+		console.log("Listing.SelectedItemsChanged. shellItems", items);
+	}
+	function handleListingSelectionChanged(items) {
+		console.log("Listing.SelectionChanged. ShellItems=", items);
+	}
+
+	function handleListingSelectNextFolder() {
+		console.log("Listing.SelectNextFolder");
+	}
+	function handleListingSelectNextObject() {
+		console.log("Listing.SelectNextObject");
+	}
+	function handleListingSelectNextObjectFile() {
+		console.log("Listing.SelectNextObjectFile");
+	}
+	function handleListingSelectPreviousFolder() {
+		console.log("Listing.SelectPreviousFolder");
+	}
+	function handleListingSelectPreviousObject() {
+		console.log("Listing.SelectPreviousObject");
+	}
+
+	function handleListingStarted() {
+		console.log("Listing.Started");
+	}
+	function handleListingStop() {
+		console.log("Listing.Stop");
+	}
+
+	var listing = dashboard.ShellFrame.ActiveListing || dashboard.ShellFrame.Listing;
+	if (listing) {
+
+		listing.Events.Register(MFiles.Event.ContentChanged, handleListingContentChanged).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ContentChanged = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.ListingActivated, handleListingListingActivated).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ListingActivated = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.ListingDeactivated, handleListingListingDeactivated).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ListingDeactivated = " + handle);
+		});
+
+		listing.Events.Register(MFiles.Event.ListItemAdded, handleListingListItemAdded).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ListItemAdded = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.ListItemModified, handleListingListItemModified).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ListItemModified = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.ListItemRemoved, handleListingListItemRemoved).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.ListItemRemoved = " + handle);
+		});
+
+		listing.Events.Register(MFiles.Event.SelectedItemsChanged, handleListingSelectedItemsChanged).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectedItemsChanged = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.SelectionChanged, handleListingSelectionChanged).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectionChanged = " + handle);
+		});
+
+		listing.Events.Register(MFiles.Event.SelectNextFolder, handleListingSelectNextFolder).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectNextFolder = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.SelectNextObject, handleListingSelectNextObject).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectNextObject = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.SelectNextObjectFile, handleListingSelectNextObjectFile).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectNextObjectFile = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.SelectPreviousFolder, handleListingSelectPreviousFolder).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectPreviousFolder = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.SelectPreviousObject, handleListingSelectPreviousObject).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.SelectPreviousObject = " + handle);
+		});
+
+		listing.Events.Register(MFiles.Event.Started, handleListingStarted).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.Started = " + handle);
+		});
+		listing.Events.Register(MFiles.Event.Stop, handleListingStop).then((handle) => {
+			hdlDBRefresh = handle;
+			console.log("registered Listing.Stop = " + handle);
+		});
+	}
 }
